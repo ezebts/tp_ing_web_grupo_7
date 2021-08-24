14f4c796c2383f0fd42c06822c29cc9e2649dec2
Ezequiel Bitschin <ezequiel.bitschin@gmail.com>
2021-08-24 00:00:27 -0300

    Corregir urls y agregar template base

--- codigo/repo_web_tesis/repo_web_tesis/urls.py
+++ codigo/repo_web_tesis/repo_web_tesis/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from sitio import views
 
 urlpatterns = [
     # Admin urls
     path('admin/', admin.site.urls),
 
     # Sitio urls
-    path('', include('sitio.urls'))
+    path('', views.inicio)
 ]
