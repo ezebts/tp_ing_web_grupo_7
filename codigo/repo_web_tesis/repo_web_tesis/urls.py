9874f87cb9316af274500882bc0c7e21889615ff
Ezequiel Bitschin <ezequiel.bitschin@gmail.com>
2021-09-11 05:08:52 -0300

    [fix] Corregir url de estaticos y media

--- codigo/repo_web_tesis/repo_web_tesis/urls.py
+++ codigo/repo_web_tesis/repo_web_tesis/urls.py
@@ -38,10 +38,5 @@ urlpatterns = [
     path('repositorio/publicacion', views.publicacion, name='publicacion'),
     path('registro/confirmar-email/<uid>/<token>',
          views.confirmar_email, name='confirmar_email')
-]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
